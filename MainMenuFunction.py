0f1011d61bf3bb86dcd5d7f28c1b2457bc07dd93
fishytehfish <dmmanhardt@gmail.com>
2018-06-19 11:29:14 -0400

    Main Menu formatting
    fixed some formatting

--- MainMenuFunction.py
+++ MainMenuFunction.py
@@ -10,10 +10,11 @@ Created on Fri May 18 15:55:15 2018
 def MainMenu():
     MainMenuValidChoice = False
     while MainMenuValidChoice == False:
-        MainMenuChoice = input('What would you like to do?\
-                               1)Enter a recipe\
-                               2)Create a food list\
-                               3)Exit')
+        MainMenuChoice = input('What would you like to do? \n\
+1)Enter a recipe \n\
+2)Create a food list \n\
+3)Exit \n\
+Your input: ')
         #choice 1 would run the EnterNewRecipe function
         if MainMenuChoice == "1":
             MainMenuValidChoice = True
@@ -26,4 +27,4 @@ def MainMenu():
             MainMenuValidChoice = True
         else:
             print("That was not a valid option, \
-                  please enter an integer for your choice")
+                  please enter an integer (1-3) for your choice")
