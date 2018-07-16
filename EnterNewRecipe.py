b89446461cfb2cf681d9e4ae99910f2060931f26
fishytehfish <dmmanhardt@gmail.com>
2018-07-16 17:25:59 -0400

    made changes
    title

--- EnterNewRecipe.py
+++ EnterNewRecipe.py
@@ -16,51 +16,41 @@ for recipe in content:
 
 #prompt user for name of recipe and see if it already exists in RecipeList, if not, run AddNewRecipe()
 def enter_new_recipe():
-    new_recipe_name = False
-    while new_recipe_name == False:
+    while  True:
         recipe_name = input("What is the name of the recipe? ")
         #check to see if the recipe is already stored in recipeList
         #and add the recipe to recipeList if it isn't, proceed to add
         #ingredients
         if recipe_name not in recipe_list:
-            new_recipe_name = True
             add_new_recipe(recipe_name)
+            break
         else:
             print("There is already a recipe with that name.")
             
 def add_new_recipe(recipe_name):
     print("Adding new recipe: ", recipe_name)
     recipe_list.append(recipe_name)
-    #prompt user to enter how many days/servings the recipe is 
-    #and assign that to a serving size variable
     define_serving_size(recipe_name)
-    #store serving size in tuple? as ((recipeName, servingSize, Ingredients),(recipeName,etc))
-    #StoreServingSize(ServingSize)
-    #once recipe is named, ask for ingredients and add to a tuple
     enter_ingredients(recipe_name)
     #once ingredients are entered, add recipes to RecipeStorage and close
     add_recipes_to_file(recipe_name)
     
 def define_serving_size(recipe_name):
-    serving_size = input("How many meals does this recipe serve for two people? ")
-    
+    serving_size = []
+    serving_size.append(input("How many meals does this recipe serve for two people? "))
     
 def enter_ingredients(recipe_name):
     print("We will now add the ingredients for", recipe_name, ", please enter each ingredient \
 individually")
     ingredients = []
-    more_ingredients_to_add = True
-    ingredient_number = 1
     #take ingredient name and add to list, will add amount later
-    while more_ingredients_to_add == True:
+    while True:
         ingredients_choice = input("Enter the name of the ingredient, \
 if no more ingredients, type 'next': ")
         if ingredients_choice == "next":
-            more_ingredients_to_add = False
             break
         else:
             ingredients.append(ingredients_choice)
-            ingredient_number += 1
             print("Your ingredients so far are: ", ingredients)
         
     #go through tuple and prompt user to add amount of each
