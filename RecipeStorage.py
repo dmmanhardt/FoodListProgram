14e34d3098946ac535706daa48a59265355beb68
fishytehfish <dmmanhardt@gmail.com>
2018-08-07 16:48:49 -0400

    made changes
    title

--- RecipeStorage.py
+++ RecipeStorage.py
@@ -15,3 +15,22 @@ def read_recipe_storage():
     for recipe in content:
         recipe_list.append(recipe)
     return recipe_list
+
+# recipe storage format = ["recipe_name: (ingredient_name: ingredient_amount: measurement, ingredient_name: ingredient_amount: measurement, etc)"]
+    
+class Recipe(object):
+    pass
+
+class Ingredient(object):
+    def __init__(self, amount, measurement):
+        self.amount = amount
+        self.measurement = measurement
+        
+# ingredients will be stored after input from user as 3 lists, ingredient_name, ingredient_amount, and ingredient_measurement
+steak_and_potatoes = Recipe()
+
+ingredient_list = ["steak", "potatoes"]
+
+for ingredient in ingredient_list:
+    steak_and_potatoes.ingredient = Ingredient(input("Serving amount? "), input("Measurement? "))
+    print(steak_and_potatoes.ingredient.amount, steak_and_potatoes.ingredient.measurement)
