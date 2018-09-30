9c1a6c8fd90bba5643a294fc75abd1362572bfeb
fishytehfish <dmmanhardt@gmail.com>
2018-09-30 14:34:03 -0400

    working up to saving selection choices

--- mealprep/create.py
+++ mealprep/create.py
@@ -44,5 +44,36 @@ def add_recipe():
 @bp.route('/select', methods=('GET', 'POST'))
 def select_recipes():
     days_for_meal_prep = session.get('days_for_meal_prep')
-    flash(days_for_meal_prep)
-    return render_template('/foodlist/selection.html')
+    meals = ['Breakfast', 'Lunch', 'Dinner']
+    recipe_df = Storage.read_recipe_storage()
+    recipe_names = Storage.add_recipe_names_to_list(recipe_df)
+    # get recipe_meals and store them and recipe_names as key:value pairs
+    # key being the meal, that way can loop through dictionary and pull
+    # out recipe_name values with correct meal key
+    meal_served = Selection.output_recipe_meal_served(
+            recipe_df, recipe_names)
+    recipe_with_meal = dict(zip(recipe_names, meal_served))
+    if request.method == 'POST':
+        picked_recipes = []
+        picked_recipes.append(request.form.get('recipe_select'))
+        print(picked_recipes)
+        session['picked_recipes'] = picked_recipes
+#        for day in days_for_meal_prep:
+#            for meal in meals:
+#                picked_recipes.append(request.form.get('recipe_select'))
+#                print(picked_recipes)
+#        session['picked_recipes'] = picked_recipes
+        error = None
+        
+        if error is not None:
+            flash(error)
+        else:
+            return redirect(url_for('create.grocery_list'))
+    return render_template('/foodlist/selection.html', meals=meals,
+                           days=days_for_meal_prep, recipes=recipe_with_meal)
+    
+@bp.route('/grocerylist', methods=('GET', 'POST'))
+def grocery_list():
+    picked_recipes = session.get('picked_recipes')
+    return render_template('/foodlist/grocerylist.html',
+                           picked_recipes=picked_recipes)
