ff2235a84b3644ac9da5d2c9879c2b9213956b5a
fishytehfish <dmmanhardt@gmail.com>
2018-10-10 16:58:44 -0400

    updated

--- mealprep/ParseRecipe.py
+++ mealprep/ParseRecipe.py
@@ -3,23 +3,40 @@ def parse_ingredient_info(ingredient_info):
     amounts = []
     measurements = []
     ingredients = []
-    measurement_units = ['lb', 'cup', 'tbsp']
+    measurement_units = {"cup": ["cups", "cup", "c.", "c"], "fluid_ounce": ["fl. oz.", "fl oz", "fluid ounce", "fluid ounces"],
+         "gallon": ["gal", "gal.", "gallon", "gallons"], "ounce": ["oz", "oz.", "ounce", "ounces"],
+         "pint": ["pt", "pt.", "pint", "pints"], "pound": ["lb", "lb.", "pound", "pounds"],
+         "quart": ["qt", "qt.", "qts", "qts.", "quart", "quarts"],
+         "tablespoon": ["tbsp.", "tbsp", "T", "T.", "tablespoon", "tablespoons", "tbs.", "tbs"],
+         "teaspoon": ["tsp.", "tsp", "t", "t.", "teaspoon", "teaspoons"],
+         "gram": ["g", "g.", "gr", "gr.", "gram", "grams"], "kilogram": ["kg", "kg.", "kilogram", "kilograms"],
+         "liter": ["l", "l.", "liter", "liters"], "milligram": ["mg", "mg.", "milligram", "milligrams"],
+         "milliliter": ["ml", "ml.", "milliliter", "milliliters"], "pinch": ["pinch", "pinches"],
+         "dash": ["dash", "dashes"], "touch": ["touch", "touches"], "handful": ["handful", "handfuls"],
+         "stick": ["stick", "sticks"], "clove": ["cloves", "clove"], "can": ["cans", "can"], "large": ["large"],
+         "small": ["small"], "scoop": ["scoop", "scoops"], "filets": ["filet", "filets"], "sprig": ["sprigs", "sprig"]}
     ingredient_info = format_ingredient_info(ingredient_info)
     for row in ingredient_info:
         amount = None
-        measurement = None
+        measurement = ''
         ingredient = None
         removal_list = []
         row = row.split()
         for word in row:
             # if int in word or if word is fraction or decimal
             if check_for_numbers(word) == True:
-                amount = word
-            elif word in measurement_units:
+                word = int(word)
+                if amount is not None:
+                    #convert fractions/decimals before adding?
+                    amount += word
+                else:
+                    amount = word
+            elif any(word in unit for unit in measurement_units.values()) == True:
                 measurement = word
-                removal_list.append(measurement)
             else:
                 continue
+        removal_list.append(amount)
+        removal_list.append(measurement)
         # remove amount and measurement from row, name is rest of row
         ingredient = [word for word in row if word not in removal_list]
         ingredient = ' '.join(ingredient)
@@ -32,12 +49,13 @@ def parse_ingredient_info(ingredient_info):
     print(amounts)
     print(measurements)
     print(ingredients)
-
     # zip together lists
     #return zipped list
     
 def format_ingredient_info(ingredient_info):
     ingredient_info = [info.strip() for info in ingredient_info.split('\n')]
+    #remove blank rows from ingredient_info
+    ingredient_info = list(filter(None, ingredient_info))
     return ingredient_info
 
 def check_for_numbers(word):
