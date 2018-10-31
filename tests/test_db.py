df5c2ae40fd8b81318b6fa3ac4deaab87bf1e266
fishytehfish <dmmanhardt@gmail.com>
2018-10-31 16:41:19 -0400

    testing

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 31 16:13:27 2018
+import sqlite3
 
-@author: MANHARDTD
-"""
+import pytest
+from mealprep.db import get_db
 
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+        
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+        
+    assert 'closed' in str(e)
+    
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+        
+    def fake_init_db():
+        Recorder.called = True
+        
+    monkeypatch.setattr('mealprep.db.init_db', fake_init_db)
+    result = runner.invoke(args=)
