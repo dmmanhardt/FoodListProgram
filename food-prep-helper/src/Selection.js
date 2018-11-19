b06f09228fd77c5aeab4aa257cb18578b2212132
fishytehfish <dmmanhardt@gmail.com>
2018-11-18 20:30:52 -0500

    updated:

--- food-prep-helper/src/Selection.js
+++ food-prep-helper/src/Selection.js
@@ -13,20 +13,71 @@ const SelectionHeader = () => {
     );
 }
 
-const SelectionDays = props => {
-    const rows = props.createOptions.map((row, index) => {
-        return (
-            <tr key={index}>
-                <td>{row.startDay}</td>
-                <td>{row.numberDays}</td>
-                <td>{index}</td>
-            </tr>
-        );
-    });
+class RecipeOption extends Component {
+    render() {
+        const { createOptions } = this.props;
 
+        return (
+            <select>
+                <option value="none"></option>
+                <ReturnRecipeOptions createOptions={createOptions} />
+            </select>
+        );
+    }
+}
+
+const ReturnRecipeOptions = props => {
+    const meals = ["Breakfast", "Lunch", "Dinner"];
+    const week = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    for (let i = 0; i < props.createOptions[0].numberDays; i++) {
+        // todo: get corresponding index of startDay from week
+        var daysToPlanFor = [week[(props.createOptions[0].startDay + 1 + i) % 7]];
+        console.log(props.createOptions[0].startDay);
+        console.log(week["Monday"]);
+    }
     return (
-        <tbody>{rows}</tbody>
+        <option>{daysToPlanFor}</option>
     );
+}
+
+class SelectionDays extends Component {
+    // change this to get days from python file (if starting on monday with 4 Days
+    // python should return Monday, Tuesday, Wednesday, Thursday), then this
+    // function should return as a row: day, dropdown, dropdown, dropdown with
+    // each dropdown containing available recipes
+    render () {
+        const { createOptions } = this.props;
+        const rows = createOptions.map((row, index) => {
+            // change the select elements to be their own components
+            return (
+                <tr key={index}>
+                    <td>{row.startDay}</td>
+                    <td>
+                        <RecipeOption createOptions={createOptions} />
+                    </td>
+                    <td>
+                        <RecipeOption createOptions={createOptions} />
+                    </td>
+                    <td>
+                        <RecipeOption createOptions={createOptions} />
+                    </td>
+                </tr>
+            );
+        });
+
+        return (
+            <tbody>{rows}</tbody>
+        );
+    }
 }
 
 class Selection extends Component {
